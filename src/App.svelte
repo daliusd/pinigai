37b8a22d3ad0a6f741f98927c4d4884ad5a698d8
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2019-08-16 16:07:46 +0300

    Show list of companies.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,40 @@
 <script>
+    import { onMount } from 'svelte';
+    import VirtualList from '@sveltejs/svelte-virtual-list';
 
+    import Company from './Company.svelte';
+
+    let data = [];
+    let promise;
+
+    async function getData(month) {
+        const response = await fetch(`/data/${month}.json`);
+        const json = await response.json();
+        data = json;
+    }
+
+    onMount(async function() {
+        promise = getData('201906');
+    });
 </script>
 
 <style>
-
+    .companies {
+        height: 100%;
+        min-width: 400px;
+    }
 </style>
 
 <h1>Pinigai</h1>
+
+{#await promise}
+    <p>Palaukite, duomenys kraunami...</p>
+{:then}
+    <div class="companies">
+        <VirtualList items={data} let:item>
+            <Company company={item} />
+        </VirtualList>
+    </div>
+{:catch error}
+    <p style="color: red">{error.message}</p>
+{/await}
---
+++ src/Company.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let company;
+</script>
+
+<style>
+    .name {
+        display: inline-block;
+        width: 40%;
+    }
+    .insured {
+        display: inline-block;
+        width: 10%;
+    }
+    .salary {
+        display: inline-block;
+        width: 10%;
+    }
+    .category {
+        display: inline-block;
+        width: 38%;
+    }
+</style>
+
+<div>
+    <span class="name">{company.n}</span>
+    <span class="insured">{company.i}</span>
+    <span class="salary">{company.w}</span>
+    <span class="category">{company.m}</span>
+</div>
